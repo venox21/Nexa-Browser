4b4befe50f9b57d61d524ac981d6bcb60d4fb11e
Fabian <fabian@nova-browser.dev>
2026-09-10 19:48:55 +0200

    release: Bump to v2.0.3 with login manager button, autofill and updated installer

--- Browser/Resources/BrandingConfig.cs
+++ Browser/Resources/BrandingConfig.cs
@@ -7,8 +7,8 @@ namespace Browser.Resources
     public static class BrandingConfig
     {
         public const string BrowserName = "Nexa";
-        public const string BrowserVersion = "2.0.2";
-        public const string BrowserDisplayVersion = "v2.0.2";
+        public const string BrowserVersion = "2.0.3";
+        public const string BrowserDisplayVersion = "v2.0.3";
 
         /// <summary>
         /// Default search engine URL template. {0} is replaced with the search query.
